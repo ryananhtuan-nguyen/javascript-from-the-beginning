f0e57684073838beafd972040d90e678b276039d
Ryan Nguyen <ryan.nguyen0720@gmail.com>
2023-10-25 17:24:20 +1300

    sliding windows

--- sliding-window-fixed-size.js
+++ sliding-window-fixed-size.js
@@ -5,7 +5,15 @@ Calculate the largest subarray sum size 3
 
 */
 
-const arr = [1, 2, 3, 5, 8, 0, 4, 2, 6, 4]
+const random_numbers = [
+  135, 764, 289, 501, 834, 176, 633, 317, 432, 71, 892, 625, 415, 298, 662, 468,
+  144, 779, 823, 41, 398, 969, 584, 259, 30, 768, 933, 75, 601, 189, 898, 451,
+  517, 246, 832, 715, 570, 97, 58, 889, 850, 952, 191, 686, 286, 84, 297, 542,
+  700, 486, 154, 525, 330, 592, 741, 127, 453, 639, 576, 136, 871, 396, 814,
+  184, 721, 617, 794, 73, 180, 936, 381, 31, 98, 737, 267, 629, 643, 210, 738,
+  981, 217, 226, 683, 244, 937, 49, 891, 498, 948, 907, 42, 819, 44, 488, 713,
+  595, 938, 866, 396, 33, 69, 621, 970,
+]
 
 // First approach, calculating every subarray of 3
 
@@ -20,7 +28,7 @@ function findLargestSubarray(arr) {
   return max
 }
 
-console.log(findLargestSubarray(arr))
+// console.log(findLargestSubarray(arr))
 //---------------------------------
 //can be also written as-----------
 //---------------------------------
@@ -32,10 +40,41 @@ function findLargestSubarray1(arr, n) {
     for (let j = 0; j < n; j++) {
       current.push(arr[i + j])
     }
-    console.log(current)
+    // console.log(current)
     const currentSum = current.reduce((a, b) => a + b, 0)
     if (currentSum > max) max = currentSum
   }
   return max
 }
-console.log(findLargestSubarray1(arr, 3))
+// console.log(findLargestSubarray1(arr, 3))
+
+//---------------------------------
+//Sliding window-------------------
+//---------------------------------
+
+function slidingWindow(arr, n) {
+  let max = 0
+  for (let i = 0; i < n; i++) {
+    max += arr[i]
+  }
+  // console.log(max)
+  for (let i = 1; i < arr.length - n; i++) {
+    const currentSum = max - arr[i] + arr[i + n]
+  }
+}
+
+// slidingWindow(random_numbers, 20)
+
+const maxSubArray = (nums) => {
+  let maxSum = -Infinity
+  let currentSum = 0
+
+  for (let i = 0; i < nums.length; i++) {
+    currentSum = Math.max(nums[i], currentSum + nums[i])
+
+    maxSum = Math.max(currentSum, maxSum)
+  }
+  return maxSum
+}
+console.log(findLargestSubarray1(random_numbers))
+console.log(maxSubArray(random_numbers))
